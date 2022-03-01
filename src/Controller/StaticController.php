f2346f522b4bf1087a35c8fb5ed9056595dcbab9
LoyGeeko <jeremy.qgn62@gmail.com>
2022-03-01 14:02:34 +0100

    first

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -8,9 +8,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StaticController extends AbstractController
 {
+    #[Route('/index', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('static/index.html.twig', [
+            'controller_name' => 'StaticController',
+        ]);
+    }
+
     #[Route('/accueil', name: 'accueil')]
     public function accueil(): Response
     {
-        return $this->render('static/accueil.html.twig', []);
+        return $this->render('static/accueil.html.twig', [
+            'controller_name' => 'StaticController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')] // étape 1
+    public function contact(): Response // étape 2
+    {
+        return $this->render('static/contact.html.twig', [ // étape 3
+            
+        ]);
     }
 }
