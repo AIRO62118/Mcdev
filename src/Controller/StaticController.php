9b8977248c0b0a438513f9a7b1a097ec5f763623
Laforest-Samuel <samuel.laforest.sio@gmail.com>
2022-03-01 14:23:22 +0100

    clean

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -8,27 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StaticController extends AbstractController
 {
-    #[Route('/index', name: 'index')]
-    public function index(): Response
-    {
-        return $this->render('static/index.html.twig', [
-            'controller_name' => 'StaticController',
-        ]);
-    }
 
     #[Route('/accueil', name: 'accueil')]
     public function accueil(): Response
     {
-        return $this->render('static/accueil.html.twig', [
-            'controller_name' => 'StaticController',
-        ]);
+        return $this->render('static/accueil.html.twig', []);
     }
 
-    #[Route('/contact', name: 'contact')] // étape 1
-    public function contact(): Response // étape 2
+     #[Route('/connexion', name: 'connexion')]
+     public function connexion(): Response
+     {
+         return $this->render('login/connexion.html.twig', []);
+     }
+
+      #[Route('/inscription', name: 'inscription')]
+    public function inscription(): Response
     {
-        return $this->render('static/contact.html.twig', [ // étape 3
-            
-        ]);
+        return $this->render('login/inscription.html.twig', []);
     }
+
+     #[Route('/paiement', name: 'paiement')]
+     public function paiement(): Response
+     {
+         return $this->render('static/paiement.html.twig', []);
+     }
+
+      #[Route('/profil', name: 'profil')]
+    public function profil(): Response
+    {
+        return $this->render('profil/profil.html.twig', []);
+    }
+
+     #[Route('/recherche', name: 'recherche')]
+     public function recherche(): Response
+     {
+         return $this->render('static/recherche.html.twig', []);
+     }
+
 }
