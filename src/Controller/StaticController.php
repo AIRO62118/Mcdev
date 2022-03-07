047fe07d301d859dc5cb3eb77ed77adcb4a9c145
AIRO62118 <fkarbowysio@gmail.com>
2022-03-07 08:33:21 +0100

    Correction

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -15,34 +15,4 @@ class StaticController extends AbstractController
         return $this->render('static/accueil.html.twig', []);
     }
 
-     #[Route('/connexion', name: 'connexion')]
-     public function connexion(): Response
-     {
-         return $this->render('login/connexion.html.twig', []);
-     }
-
-      #[Route('/inscription', name: 'inscription')]
-    public function inscription(): Response
-    {
-        return $this->render('login/inscription.html.twig', []);
-    }
-
-     #[Route('/paiement', name: 'paiement')]
-     public function paiement(): Response
-     {
-         return $this->render('static/paiement.html.twig', []);
-     }
-
-      #[Route('/profil', name: 'profil')]
-    public function profil(): Response
-    {
-        return $this->render('profil/profil.html.twig', []);
-    }
-
-     #[Route('/recherche', name: 'recherche')]
-     public function recherche(): Response
-     {
-         return $this->render('static/recherche.html.twig', []);
-     }
-
 }
