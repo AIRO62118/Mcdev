5ae4fd5de6403c2cea9a2292cb8e4cda9a4dfc49
AIRO62118 <fkarbowysio@gmail.com>
2022-03-07 10:19:02 +0100

    Squelette Site

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -42,7 +42,7 @@ class StaticController extends AbstractController
                     'sujet'=> $contact->getSujet(),
                     'message'=> $contact->getMessage()
                 ]);
-
+                $contact->setDateEnvoi(new \Datetime());
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($contact);
                 $em->flush();
