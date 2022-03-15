7e78e4e3c5f379195219192e58ef7d8d2ed94330
Mehdi le OUI <mehdi.raymond1@outlook.fr>
2022-03-15 14:57:52 +0100

    147852369

--- migrations/Version20220315135431.php
+++ migrations/Version20220315135431.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220315074757 extends AbstractMigration
+final class Version20220315135431 extends AbstractMigration
 {
     public function getDescription(): string
     {
