e168d414545b30963467fbd26325753487094c89
Laforest-Samuel <samuel.laforest.sio@gmail.com>
2022-03-15 12:37:21 +0100

    info profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
     public function profil(): Response
     {
-        return $this->render('profil/profil.html.twig', []);
+        $liste = $this->getDoctrine()->getRepository(User::class)->users(5);
+        return $this->render('profil/profil.html.twig', ["liste"=>$liste]);
     }
 }
