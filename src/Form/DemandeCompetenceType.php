3561b833ec764aed8fbe7f5f31c85462fb59501f
AIRO62118 <fkarbowysio@gmail.com>
2022-03-20 18:31:46 +0100

    Modif FormDemande

--- src/Form/DemandeCompetenceType.php
+++ src/Form/DemandeCompetenceType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
@@ -19,7 +21,15 @@ class DemandeCompetenceType extends AbstractType
     {
         $builder
             ->add('salaire')
-            ->add('niveau_demande')
+            ->add('niveau_demande', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+            ])
             ->add('competence',EntityType::class, [
                 'class' => Competence::class,
                 'choice_label' => 'libelle',
